d97490b0067f9925bc07a9ca2d3b8d9f7033a71f
Ryan Erricson <ryanerricson@gmail.com>
2021-04-16 11:47:11 -0400

    commit tree 361092dab969221b5924ed7a30db13f3d77c5d98

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  
+  def new
+  end
+
+  def create
+    session[:name] = params[:name]
+    if session[:name] == "" || session[:name] == nil
+    redirect_to '/login'
+    else
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.delete :name
+  end 
+  
+end
